5a7271e45b2c7b6d0617b0185bf58a187734ab0f
Alba Maitia Vadillo Valderrama <alba.vadillo3@gmail.com>
2025-03-26 15:31:27 +0100

    sentiment

---
+++ 05-Fine_Tuning_BERT_for_Sentiment/sentiment_classification.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+#url = "https://raw.githubusercontent.com/laxmimerit/All-CSV-ML-Data-Files-Download/master/twitter_multi_class_sentiment.csv"
+#df = pd.read_csv(url)
+#df.to_csv("assets/twitter_sentiment.csv", index=False) # Guardar el archivo localmente
+df = pd.read_csv("assets/twitter_sentiment.csv")  # En Windows
+
+
